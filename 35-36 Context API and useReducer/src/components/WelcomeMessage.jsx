0496095ef5492be036d0b22ce4c2fcc64548b7a8
karthik <karthikb1010@gmail.com>
2025-12-27 13:04:58 +0530

    chnage

--- 35-36 Context API and useReducer/src/components/WelcomeMessage.jsx
+++ 35-36 Context API and useReducer/src/components/WelcomeMessage.jsx
@@ -5,9 +5,7 @@ import { TodoItemsContext } from "../store/todo-items-store";
 const WelcomeMessage = () => {
   const { todoItems } = useContext(TodoItemsContext);
 
-  return (
-    todoItems.length === 0 && <p className={styles.welcome}>Enjoy Your Day</p>
-  );
+  return todoItems.length === 0 && <p className={styles.welcome}> Hello </p>;
 };
 
 export default WelcomeMessage;
