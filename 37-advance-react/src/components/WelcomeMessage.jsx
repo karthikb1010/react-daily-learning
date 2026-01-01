dea11fca77531f40b529c6463ae56e51d035bb5d
karthik <karthikb1010@gmail.com>
2026-01-01 23:26:33 +0530

    Advance React

---
+++ 37-advance-react/src/components/WelcomeMessage.jsx
@@ -0,0 +1,16 @@
+const WelcomeMessage = ({ onGetPostsClick }) => {
+  return (
+    <center className="welcome-message">
+      <h1>There are no Posts</h1>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={onGetPostsClick}
+      >
+        Get Posts From Server
+      </button>
+    </center>
+  );
+};
+
+export default WelcomeMessage;
