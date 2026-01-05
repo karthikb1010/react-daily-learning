cd9d7b06be909ea5c14073b7a4a7c49db34826de
karthik <karthikb1010@gmail.com>
2026-01-05 21:50:51 +0530

    8-bharat clock

--- Projects/8-bharat-clock-version-two(after40-useEffect)/src/components/CurrentTime.jsx
+++ Projects/8-bharat-clock-version-two(after40-useEffect)/src/components/CurrentTime.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+
 let CurrentTime = () => {
-  let time = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <p className="lead">
