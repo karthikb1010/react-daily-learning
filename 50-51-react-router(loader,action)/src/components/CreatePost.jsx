c7eaee839f854ea4aefeb937f773036fbbec97d2
karthik <karthikb1010@gmail.com>
2026-01-06 17:47:24 +0530

    loader and action

--- 50-51-react-router(loader,action)/src/components/CreatePost.jsx
+++ 50-51-react-router(loader,action)/src/components/CreatePost.jsx
@@ -1,61 +1,17 @@
-import { useContext, useRef } from "react";
-import { PostList } from "../store/post-list-store";
-import { useNavigate } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
 
 const CreatePost = () => {
-  const { addPost } = useContext(PostList);
-  const navigate = useNavigate();
-
-  const userIdElement = useRef();
-  const postTitleElement = useRef();
-  const postBodyElement = useRef();
-  const reactionsElement = useRef();
-  const tagsElement = useRef();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const userId = userIdElement.current.value;
-    const postTitle = postTitleElement.current.value;
-    const postBody = postBodyElement.current.value;
-    const reactions = {
-      likes: Number(reactionsElement.current.value),
-      dislikes: 0,
-    };
-    const tags = tagsElement.current.value.split(" ");
-
-    userIdElement.current.value = "";
-    postTitleElement.current.value = "";
-    postBodyElement.current.value = "";
-    reactionsElement.current.value = "";
-    tagsElement.current.value = "";
-
-    fetch("https://dummyjson.com/posts/add", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: postTitle,
-        body: postBody,
-        reactions: reactions,
-        userId: userId,
-        tags: tags,
-      }),
-    })
-      .then((res) => res.json())
-      .then((post) => {
-        addPost(post);
-        navigate("/");
-      });
-  };
+  // const { addPost } = useContext(PostList);
 
   return (
-    <form className="create-post" onSubmit={handleSubmit}>
+    <Form method="POST" className="create-post">
       <div className="mb-3">
         <label htmlFor="userId" className="form-label">
           Enter your User Id here
         </label>
         <input
           type="text"
-          ref={userIdElement}
+          name="userId"
           className="form-control"
           id="userId"
           placeholder="Your User Id"
@@ -68,7 +24,7 @@ const CreatePost = () => {
         </label>
         <input
           type="text"
-          ref={postTitleElement}
+          name="title"
           className="form-control"
           id="title"
           placeholder="How are you feeling today..."
@@ -81,7 +37,7 @@ const CreatePost = () => {
         </label>
         <textarea
           type="text"
-          ref={postBodyElement}
+          name="body"
           rows="4"
           className="form-control"
           id="body"
@@ -95,7 +51,7 @@ const CreatePost = () => {
         </label>
         <input
           type="text"
-          ref={reactionsElement}
+          name="reactions"
           className="form-control"
           id="reactions"
           placeholder="How many people reacted to this post"
@@ -110,7 +66,7 @@ const CreatePost = () => {
           type="text"
           className="form-control"
           id="tags"
-          ref={tagsElement}
+          name="tags"
           placeholder="Please enter tags using space"
         />
       </div>
@@ -118,8 +74,25 @@ const CreatePost = () => {
       <button type="submit" className="btn btn-primary">
         Post
       </button>
-    </form>
+    </Form>
   );
 };
+
+export async function createPostAction(data) {
+  const formData = await data.request.formData();
+  const postData = Object.fromEntries(formData);
+  postData.tags = postData.tags.split(" ");
+  console.log(postData);
+  fetch("https://dummyjson.com/posts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(postData),
+  })
+    .then((res) => res.json())
+    .then((post) => {
+      addPost(post);
+    });
+  return redirect("/");
+}
 
 export default CreatePost;
--- 50-51-react-router(loader,action)/src/main.jsx
+++ 50-51-react-router(loader,action)/src/main.jsx
@@ -2,7 +2,7 @@ import { Children, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./routes/App.jsx";
-import CreatePost from "./components/CreatePost.jsx";
+import CreatePost, { createPostAction } from "./components/CreatePost.jsx";
 import PostList, { postLoader } from "./components/PostList.jsx";
 
 const router = createBrowserRouter([
@@ -11,7 +11,11 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: "/", element: <PostList />, loader: postLoader },
-      { path: "/create-post", element: <CreatePost /> },
+      {
+        path: "/create-post",
+        element: <CreatePost />,
+        action: createPostAction,
+      },
     ],
   },
 ]);
