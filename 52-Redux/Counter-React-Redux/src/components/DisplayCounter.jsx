da223f890ae1011b4c5e12c8998488c9e9efe365
karthik <karthikb1010@gmail.com>
2026-01-10 01:09:16 +0530

    UPDATE

--- 52-Redux/Counter-React-Redux/src/components/DisplayCounter.jsx
+++ 52-Redux/Counter-React-Redux/src/components/DisplayCounter.jsx
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 const DisplayCounter = () => {
-  return <p className="lead mb-4">Counter Value:</p>;
+  const counter = useSelector((store) => store.counter);
+
+  return <p className="lead mb-4">Counter Value: {counter}</p>;
 };
 export default DisplayCounter;
--- 52-Redux/Counter-React-Redux/src/store/index.js
+++ 52-Redux/Counter-React-Redux/src/store/index.js
@@ -1,6 +1,19 @@
 import { createStore } from "redux";
 
-const counterReducer = (store, action) => {
+const INITIAL_VALUE = {
+  counter: 0,
+};
+
+const counterReducer = (store = INITIAL_VALUE, action) => {
+  if (action.type === "INCREMENT") {
+    return { counter: store.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { counter: store.counter - 1 };
+  } else if (action.type === "ADD") {
+    return { counter: store.counter + Number(action.payload.num) };
+  } else if (action.type === "SUBTRACT") {
+    return { counter: store.counter - Number(action.payload.num) };
+  }
   return store;
 };
 
